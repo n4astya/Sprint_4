e2794a6e959f8abef0ea48532a5b7453d9273e1e
n4asty <n4asty@stacksoft.ru>
2023-08-03 23:16:04 +0300

    add results

--- tests/test_question.py
+++ tests/test_question.py
@@ -1,13 +1,10 @@
 from pages.question_page import PageMain as P
-from locators.locators_question import MainPageLocators as L
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
 import pytest
 import allure
 
 
 class TestRun:
-    @allure.title('Проверка возможности увидеть ответы на вопросы')  # декораторы
+    @allure.title('Проверка возможности увидеть ответы на вопросы')
     @allure.description('На странице ищем вопрос, кликаем на него и проверяем, что открывается ответ на вопрос')
     @pytest.mark.parametrize('num', ['0', '1', '2', '3', '4', '5', '6', '7'])
     def test_get_answer(self, driver, num):
@@ -15,10 +12,7 @@ class TestRun:
         driver.get('https://qa-scooter.praktikum-services.ru/')
 
         main_page = P()
-        element = driver.find_element(*L.question_live_after_MKAD)
-        driver.execute_script("arguments[0].scrollIntoView();", element)
-        WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located(L.question_live_after_MKAD))
 
+        main_page.scroll(driver)
         main_page.click_question(driver, num)
         main_page.check_click_question(driver, num)
-
